d1da05b9af1fe58ae31a27ff716f6c1935357963
entl <maxvorobyov01@gmail.com>
2026-01-04 19:02:18 +0200

    feat: electron setup--date=1 day ago

---
+++ frontend/src/electron/main.ts
@@ -0,0 +1,21 @@
+import {app, BrowserWindow} from 'electron';
+import path from 'path';
+import { isDev } from './utils.js';
+
+
+type test = string;
+
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  if (isDev()) {
+    mainWindow.loadURL('http://localhost:5123');
+    return;
+  }
+  else {
+    mainWindow.loadFile(path.join(app.getAppPath(), 'dist-react', 'index.html'));
+  }
+});
